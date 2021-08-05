3d4ab89b765f7d670245ac9c6b74fd34db143fcc
Gary Briggs <gbriggs@rand.org>
2021-08-04 20:20:36 -0700

    Stuff

--- raytracer.sql
+++ raytracer.sql
@@ -14,9 +14,11 @@ UPDATE sphere SET radius2 = radius*radius WHERE radius2 IS NULL;
 DROP TABLE IF EXISTS camera;
 CREATE TABLE camera (cameraid INTEGER PRIMARY KEY,
   x REAL NOT NULL, y REAL NOT NULL, z REAL NOT NULL,
-  rot_x REAL NOT NULL, rot_y REAL NOT NULL, rot_z REAL NOT NULL, fov_rad REAL NOT NULL,
+  rot_x REAL NOT NULL, rot_y REAL NOT NULL, rot_z REAL NOT NULL,
+  fov_rad_x REAL NOT NULL, fov_rad_y REAL NOT NULL,
   max_ray_depth INTEGER);
-INSERT INTO camera (x, y, z, rot_x, rot_y, rot_z, fov_rad, max_ray_depth) VALUES (0.0, 0.0, -40.0, 0.0, 0.0, 0.0, PI()/2.0, 2);
+INSERT INTO camera (x, y, z, rot_x, rot_y, rot_z, fov_rad_x, fov_rad_y, max_ray_depth)
+  VALUES (0.0, 0.0, -40.0, 0.0, 0.0, 0.0, PI()/2.0, PI()/2.0, 2);
 
 DROP TABLE IF EXISTS img;
 CREATE TABLE img (res_x INTEGER NOT NULL, res_y INTEGER NOT NULL);
@@ -27,19 +29,45 @@ CREATE VIEW rays AS
     WITH RECURSIVE xs AS (SELECT 0 AS u, 0.0 AS img_frac_x UNION ALL SELECT u+1, (u+1.0)/img.res_x FROM xs, img WHERE xs.u<img.res_x-1),
      ys AS (SELECT 0 AS v, 0.0 AS img_frac_y UNION ALL SELECT v+1, (v+1.0)/img.res_y FROM ys, img WHERE ys.v<img.res_y-1),
      rays(img_x, img_y, depth, max_ray_depth, ray_col, x1, y1, z1, dir_x, dir_y, dir_z, x2, y2, z2, ray_len, hit_light) AS
+        -- Send out initial set of rays from camera
          (SELECT xs.u, ys.v, 0, max_ray_depth, NULL, c.x, c.y, c.z,
-                SIN(-(fov_rad/2.0)+img_frac_x*fov_rad), SIN(-(fov_rad/2.0)+img_frac_y*fov_rad), 1.0,
-                 c.x + SIN(-(fov_rad/2.0)+img_frac_x*fov_rad), c.y + SIN(-(fov_rad/2.0)+img_frac_y*fov_rad), z + 1.0,
+                SIN(-(fov_rad_x/2.0)+img_frac_x*fov_rad_x), SIN(-(fov_rad_y/2.0)+img_frac_y*fov_rad_y), 1.0,
+                 c.x + SIN(-(fov_rad_x/2.0)+img_frac_x*fov_rad_x), c.y + SIN(-(fov_rad_y/2.0)+img_frac_y*fov_rad_y), z + 1.0,
                  0.0, 0
               FROM camera c, img, xs, ys
         UNION ALL
+         -- Collide all rays with spheres
           SELECT img_x, img_y, depth+1, max_ray_depth, sphere_col, x1, y1, z1, dir_x, dir_y, dir_z, x2, y2, z2,
                  SQRT((cx-x1)*(cx-x1) + (cy-y1)*(cy-y1) + (cz-z1)*(cz-z1)), -- distance to center. fixme should be distance to intersection
                  s.is_light
+
+         -- double hit_sphere(const point3& center, double radius, const ray& r) {
+         --     vec3 oc = r.origin() - center;
+         --            -- x1-cx, y1-cy, z1-cz
+         --     auto a = dot(r.direction(), r.direction());
+         --            -- dir_x*dir_x + dir_y*dir_y + dir_z*dir_z
+         --     auto b = 2.0 * dot(oc, r.direction());
+         --            -- 2.0 * ((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z)
+         --     auto c = dot(oc, oc) - radius*radius;
+         --            -- (x1-cx)*(x1-cx) + (y1-cy)*(y1-cy) + (z1-cz)*(z1-cz)
+         --     auto discriminant = b*b - 4*a*c;
+         --            -- (2.0 * ((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z))*(2.0 * ((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z))
+         --            --    - 4 * (dir_x*dir_x + dir_y*dir_y + dir_z*dir_z) * ((x1-cx)*(x1-cx) + (y1-cy)*(y1-cy) + (z1-cz)*(z1-cz))
+         --     if (discriminant < 0) {
+         --         return -1.0;
+         --     } else {
+         --         return (-b - sqrt(discriminant) ) / (2.0*a);
+         --            -- -(2.0 * ((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z))
+         --            --   - SQRT(2.0 * ((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z))*(2.0 * ((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z))
+         --            --    - 4 * (dir_x*dir_x + dir_y*dir_y + dir_z*dir_z) * ((x1-cx)*(x1-cx) + (y1-cy)*(y1-cy) + (z1-cz)*(z1-cz))) /
+        --            --    (2.0*(dir_x*dir_x + dir_y*dir_y + dir_z*dir_z))
+         --     }
+         -- }
            FROM rays r
            LEFT JOIN sphere s ON
                -- https://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
                -- d=len( circ_center-p1 X circ_center-p2) / len(dir_x, dir_y, dir_z)
+               -- This is "does this ray collide"
              s.radius2 >
                   ((((cy-y1)*(cz-z2)-(cz-z1)*(cy-y2))*((cy-y1)*(cz-z2)-(cz-z1)*(cy-y2)) +
                     ((cz-z1)*(cx-x2)-(cx-x1)*(cz-z2))*((cz-z1)*(cx-x2)-(cx-x1)*(cz-z2)) +
@@ -50,7 +78,8 @@ CREATE VIEW rays AS
 
 DROP VIEW IF EXISTS do_render;
 CREATE VIEW do_render AS
- SELECT A.img_x, A.img_y, COALESCE(MAX(A.ray_col * 1.0/A.depth), 0.5+0.5*(A.dir_y/(SQRT(A.dir_x*A.dir_x + A.dir_y*A.dir_y + A.dir_z*A.dir_z)))) AS col
+ SELECT A.img_x, A.img_y, COALESCE(MAX(A.ray_col * 1.0/A.depth),
+       0.5+0.5*(A.dir_y/(SQRT(A.dir_x*A.dir_x + A.dir_y*A.dir_y + A.dir_z*A.dir_z)))) AS col
     FROM rays A LEFT JOIN rays B ON A.img_x=B.img_x AND A.img_y=B.img_y AND A.ray_len_idx=1 AND A.depth=B.depth-1
     GROUP BY A.img_y, A.img_x
     ORDER BY A.img_y, A.img_x;
