ac6af73a71edc5c936013a4ff8ac234cd20d4257
Gary Briggs <gbriggs@rand.org>
2021-08-17 07:50:08 -0700

    Use psql's timer instead of command line

--- create.sh
+++ create.sh
@@ -3,13 +3,14 @@
 PGPASSWORD=raytracer
 export PGPASSWORD
 
-time psql \
+psql \
 	--host=mills \
 	--port=5432 \
 	--username=raytracer \
 	--dbname=raytracer \
 	--file=setup.sql \
 	--file=raytracer.sql \
+	--command="\\timing" \
 	--command="\\copy (select * from ppm) to './pgimg.ppm' csv"
 
 xdg-open pgimg.ppm
