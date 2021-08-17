8f295ccea5652c23e21dc075cabd2bcb9166cc9e
Gary Briggs <gbriggs@rand.org>
2021-08-16 23:39:49 -0700

    Rebuild db before rendering

--- create.sh
+++ create.sh
@@ -8,6 +8,7 @@ time psql \
 	--port=5432 \
 	--username=raytracer \
 	--dbname=raytracer \
+	--file=raytracer.sql \
 	--command="\\copy (select * from ppm) to './pgimg.ppm' csv"
 
 xdg-open pgimg.ppm
