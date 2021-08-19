417bd7b1e46831e29bfcbeb55e60fff9e7f47c8c
Gary Briggs <chunky@icculus.org>
2021-08-19 12:31:24 -0700

    Allow multiple scenes to be configured.  Diffuse scattering

--- raytracer.sql
+++ raytracer.sql
@@ -1,21 +1,29 @@
+DROP TABLE IF EXISTS sphere_sample CASCADE;
+CREATE TABLE IF NOT EXISTS sphere_sample (x DOUBLE PRECISION NOT NULL, y DOUBLE PRECISION NOT NULL, z DOUBLE PRECISION NOT NULL,
+	a DOUBLE PRECISION NOT NULL, b DOUBLE PRECISION NOT NULL, c DOUBLE PRECISION NOT NULL, sampleno INTEGER NOT NULL, n_samples INTEGER NOT NULL);
+INSERT INTO sphere_sample
+     WITH square_sample AS (SELECT 2.0*(RANDOM() - 0.5) AS a1, 2.0*(RANDOM() - 0.5) AS b1, 2.0*(RANDOM() - 0.5) AS c1
+          FROM generate_series(1, 5000)),
+     ball_sample AS (SELECT a1 AS a, b1 AS b, c1 AS c, SQRT(a1*a1+b1*b1+c1*c1) AS radius FROM square_sample WHERE 1>=(a1*a1+b1*b1+c1*c1)),
+     sphere_sample AS (SELECT a/radius AS x, b/radius AS y, c/radius AS z, a, b, c, ROW_NUMBER() OVER () AS sampleno, COUNT(*) OVER () AS n_samples FROM ball_sample)
+     SELECT x,y,z,a,b,c,sampleno,n_samples FROM sphere_sample;
+DROP INDEX IF EXISTS idx_ss;
+CREATE INDEX IF NOT EXISTS idx_ss ON sphere_sample(n_samples, sampleno);
+
 DROP VIEW IF EXISTS rays CASCADE;
 CREATE VIEW rays AS
     WITH RECURSIVE
      xs AS (SELECT 0 AS u, 0.0 AS img_frac_x UNION ALL SELECT u+1, (u+1.0)/img.res_x FROM xs, img WHERE xs.u<img.res_x-1),
      ys AS (SELECT 0 AS v, 0.0 AS img_frac_y UNION ALL SELECT v+1, (v+1.0)/img.res_y FROM ys, img WHERE ys.v<img.res_y-1),
      px_sample_n(px_sample_n) AS (SELECT 1 UNION ALL SELECT px_sample_n+1 FROM px_sample_n, camera WHERE px_sample_n<camera.samples_per_px),
-     square_sample AS (SELECT 2.0*(RANDOM() - 0.5) AS a1, 2.0*(RANDOM() - 0.5) AS b1, 2.0*(RANDOM() - 0.5) AS c1
-          FROM generate_series(1, 10000)),
-     ball_sample AS (SELECT a1 AS a, b1 AS b, c1 AS c, SQRT(a1*a1+b1*b1+c1*c1) AS radius FROM square_sample WHERE 1>=(a1*a1+b1*b1+c1*c1)),
-     sphere_sample AS (SELECT a/radius AS x, b/radius AS y, c/radius AS z, a, b, c, ROW_NUMBER() OVER () AS sampleno, COUNT(*) OVER () AS n_samples FROM ball_sample),
-     rs(img_x, img_y, depth, max_ray_depth, samples_per_px, px_sample_n, color_mult,
+     rs(img_x, img_y, sceneid, depth, max_ray_depth, samples_per_px, px_sample_n, color_mult,
           ray_col_r, ray_col_g, ray_col_b,
           x1, y1, z1,
           dir_x, dir_y, dir_z,
           dir_lensquared,
           stop_tracing, ray_len_idx, hit_sphereid) AS
         -- Send out initial set of rays from camera
-         (SELECT xs.u, ys.v, -1, max_ray_depth, samples_per_px, px_sample_n, 2.0,
+         (SELECT xs.u, ys.v, c.sceneid, -1, max_ray_depth, samples_per_px, px_sample_n, 2.0,
                 CAST(NULL AS DOUBLE PRECISION), CAST(NULL AS DOUBLE PRECISION), CAST(NULL AS DOUBLE PRECISION),
                  c.x, c.y, c.z,
                  SIN(-(fov_rad_x/2.0)+img_frac_x*fov_rad_x) + 2.0 * (RANDOM()-0.5) * (fov_rad_x/res_x),
@@ -27,7 +35,7 @@ CREATE VIEW rays AS
               FROM camera c, img, xs, ys, px_sample_n
         UNION ALL
          -- Collide all rays with spheres
-          SELECT img_x, img_y, depth+1, max_ray_depth, samples_per_px, px_sample_n,
+          SELECT img_x, img_y, rs.sceneid, depth+1, max_ray_depth, samples_per_px, px_sample_n,
                  (CASE WHEN is_mirror THEN 0.95 ELSE 0.5 END)*color_mult,
                  CASE WHEN discrim>0 THEN (CASE
                                                 WHEN shade_normal THEN mat_col_r*(1+norm_x/norm_len)
@@ -68,6 +76,7 @@ CREATE VIEW rays AS
                         -SQRT(ABS(((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z) * ((x1-cx)*dir_x + (y1-cy)*dir_y + (z1-cz)*dir_z)
                             - dir_lensquared * ((x1-cx)*(x1-cx) + (y1-cy)*(y1-cy) + (z1-cz)*(z1-cz) - radius2)) / dir_lensquared)) t
                          FROM sphere s
+                         WHERE s.sceneid=rs.sceneid
                        ) hit_sphere ON discrim>0 AND t>0
            LEFT JOIN LATERAL
                (SELECT x1+dir_x*t AS hit_x, y1+dir_y*t AS hit_y, z1+dir_z*t AS hit_z,
@@ -85,7 +94,7 @@ CREATE VIEW rays AS
                (SELECT x, y, z,
                        x+norm_x/norm_len AS diffuse_dir_x, y+norm_y/norm_len AS diffuse_dir_y, z+norm_z/norm_len AS diffuse_dir_z,
                        SQRT((x+norm_x/norm_len)*(x+norm_x/norm_len)+(y+norm_y/norm_len)*(y+norm_y/norm_len)+(z+norm_z/norm_len)*(z+norm_z/norm_len)) AS diffuse_dir_len
-                FROM sphere_sample ss WHERE ss.sampleno=1+FLOOR(ABS(n_samples*norm_x/norm_len))
+                FROM sphere_sample ss WHERE ss.sampleno=1+FLOOR(n_samples*ABS((10000*norm_x)-FLOOR(10000*norm_x)))
                ) diffuse_scatter ON norm_x IS NOT NULL
               WHERE depth<max_ray_depth AND NOT stop_tracing AND ray_len_idx=1
              )
--- setup.sql
+++ setup.sql
@@ -7,30 +7,45 @@ INSERT INTO material (name, mat_col_r, mat_col_g, mat_col_b, is_metal, shade_nor
     ('red', 0.95, 0.0, 0.0, FALSE, TRUE, FALSE),
     ('green', 0.0, 0.95, 0.0, FALSE, TRUE, FALSE),
     ('blue', 0.0, 0.0, 0.95, TRUE, TRUE, FALSE),
-    ('grey', 0.0, 0.0, 0.0, FALSE, FALSE, FALSE),
+    ('grey', 0.1, 0.1, 0.1, FALSE, FALSE, FALSE),
     ('bright', 1.0, 1.0, 1.0, TRUE, TRUE, FALSE),
     ('mirror', NULL, NULL, NULL, FALSE, FALSE, TRUE),
     ('bluemirror', 0.0, 0.0, 0.02, FALSE, FALSE, TRUE),
     ('greenmirror', 0.0, 0.02, 0.0, FALSE, FALSE, TRUE)
 ;
 
+DROP TABLE IF EXISTS scene CASCADE;
+CREATE TABLE IF NOT EXISTS scene (sceneid SERIAL PRIMARY KEY,
+   scenename TEXT UNIQUE NOT NULL);
+INSERT INTO scene (scenename) VALUES ('onegreyball'),
+                                     ('reflectiontest');
+
 DROP TABLE IF EXISTS sphere CASCADE;
-CREATE TABLE sphere (sphereid SERIAL,
+CREATE TABLE sphere (sphereid SERIAL, sceneid INTEGER NOT NULL REFERENCES scene(sceneid),
   cx DOUBLE PRECISION NOT NULL, cy DOUBLE PRECISION NOT NULL, cz DOUBLE PRECISION NOT NULL,
   radius DOUBLE PRECISION NOT NULL, radius2 DOUBLE PRECISION, materialid INTEGER NOT NULL REFERENCES material(materialid) DEFERRABLE);
-INSERT INTO sphere (cx, cy, cz, radius, materialid) VALUES
+INSERT INTO sphere (cx, cy, cz, radius, materialid, sceneid) VALUES
 --     (9, 9, -10, 5, (SELECT materialid FROM material WHERE name='dark')),
 --     (-5, 7, 17, 7, (SELECT materialid FROM material WHERE name='mirror')),
 --     (15, -15, -1, 4, (SELECT materialid FROM material WHERE name='green')),
 --     (-2, -3, 8, 10, (SELECT materialid FROM material WHERE name='mirror')),
 --     (-15, -3, -15, 2, (SELECT materialid FROM material WHERE name='bright')),
+--
+(0, 24, -10, 5,
+   (SELECT materialid FROM material WHERE name='bright'), (SELECT sceneid FROM scene WHERE scenename='reflectiontest')),
+(0, 5, 0, 5,
+   (SELECT materialid FROM material WHERE name='red'), (SELECT sceneid FROM scene WHERE scenename='reflectiontest')),
+(-17, 15, -30, 15,
+   (SELECT materialid FROM material WHERE name='bluemirror'), (SELECT sceneid FROM scene WHERE scenename='reflectiontest')),
+(24, 23, 10, 23,
+   (SELECT materialid FROM material WHERE name='greenmirror'), (SELECT sceneid FROM scene WHERE scenename='reflectiontest')),
+(0, -1250, 0, 1250,
+   (SELECT materialid FROM material WHERE name='green'), (SELECT sceneid FROM scene WHERE scenename='reflectiontest')),
 
-(0, 24, -10, 5, (SELECT materialid FROM material WHERE name='bright')),
-(0, 5, 0, 5, (SELECT materialid FROM material WHERE name='red')),
-(-17, 15, -30, 15, (SELECT materialid FROM material WHERE name='bluemirror')),
-(24, 23, 10, 23, (SELECT materialid FROM material WHERE name='greenmirror')),
-
-(0, -1250, 0, 1220, (SELECT materialid FROM material WHERE name='green'))
+(0, -1250, 0, 1250,
+   (SELECT materialid FROM material WHERE name='grey'), (SELECT sceneid FROM scene WHERE scenename='onegreyball')),
+(0, 25, 0, 25,
+   (SELECT materialid FROM material WHERE name='grey'), (SELECT sceneid FROM scene WHERE scenename='onegreyball'))
 ;
 -- INSERT INTO sphere (cx, cy, cz, radius, materialid)
 -- SELECT (RANDOM()-0.5) * 100, 10 + RANDOM() * 5, (RANDOM()-0.5) * 100, RANDOM() * 12,
@@ -39,15 +54,15 @@ INSERT INTO sphere (cx, cy, cz, radius, materialid) VALUES
 UPDATE sphere SET radius2 = radius*radius WHERE radius2 IS NULL;
 
 DROP TABLE IF EXISTS camera CASCADE;
-CREATE TABLE camera (cameraid INTEGER PRIMARY KEY,
+CREATE TABLE camera (cameraid INTEGER PRIMARY KEY, sceneid INTEGER NOT NULL REFERENCES scene(sceneid),
   x DOUBLE PRECISION NOT NULL, y DOUBLE PRECISION NOT NULL, z DOUBLE PRECISION NOT NULL,
   rot_x DOUBLE PRECISION NOT NULL, rot_y DOUBLE PRECISION NOT NULL, rot_z DOUBLE PRECISION NOT NULL,
   fov_rad_x DOUBLE PRECISION NOT NULL, fov_rad_y DOUBLE PRECISION NOT NULL,
   max_ray_depth INTEGER NOT NULL, samples_per_px INTEGER NOT NULL);
-INSERT INTO camera (cameraid, x, y, z, rot_x, rot_y, rot_z, fov_rad_x, fov_rad_y, max_ray_depth, samples_per_px)
-  VALUES (1.0, 0.0, 0.0, -120.0, 0.0, 0.0, 0.0, PI()/3.0, PI()/3.0, 8, 20);
+INSERT INTO camera (cameraid, x, y, z, rot_x, rot_y, rot_z, fov_rad_x, fov_rad_y, max_ray_depth, samples_per_px, sceneid)
+  VALUES (1.0, 0.0, 15.0, -120.0, 0.0, 0.0, 0.0, PI()/3.0, PI()/3.0,
+          15, 20, (SELECT sceneid FROM scene WHERE scenename='reflectiontest'));
 
 DROP TABLE IF EXISTS img CASCADE;
 CREATE TABLE img (res_x INTEGER NOT NULL, res_y INTEGER NOT NULL, gamma DOUBLE PRECISION);
-    INSERT INTO img (res_x, res_y, gamma) VALUES (850, 850, 1.0);
-
+    INSERT INTO img (res_x, res_y, gamma) VALUES (250, 250, 1.0);
